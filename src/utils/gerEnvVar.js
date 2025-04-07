d04eee5f19baf0766f58081fd56cbfff2329babb
Angelina Simora <angelinasimora997@gmail.com>
2025-04-07 19:50:45 +0200

    rename getEnvVar-gerEnvVar

--- src/utils/gerEnvVar.js
+++ src/utils/gerEnvVar.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-export const getEnvVar = (name, defaultValue) => {
+export const gerEnvVar = (name, defaultValue) => {
     const value = process.env[name];
     if (value) return value;
     if (defaultValue) return defaultValue; throw new Error(`Cannot find procces.env[${name}]`);
